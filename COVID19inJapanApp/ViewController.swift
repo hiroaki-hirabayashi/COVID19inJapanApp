06388698f217c9793db32970d12f1d6f34394a15
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-05-04 21:05:13 +0900

    Initial Commit

---
+++ COVID19inJapanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19inJapanApp
+//
+//  Created by 平林 宏淳 on 2021/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
