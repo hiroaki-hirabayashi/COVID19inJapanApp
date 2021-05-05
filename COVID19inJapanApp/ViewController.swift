44f42d312bcd9afae6eb9d8fb177d6345749b533
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-05-05 20:40:55 +0900

    トップ画面 / 中央部のView

--- COVID19inJapanApp/ViewController.swift
+++ COVID19inJapanApp/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        seyUpGradation()
+        setUpContent()
+       
+    }
+    
+    func seyUpGradation() {
+        // 画面上部
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height / 2 )
         // グラデーション色設定
@@ -27,7 +34,27 @@ class ViewController: UIViewController {
          CG = CoreGraphics UIColorとは別の色の型
          UIColor → CGColor型へ
          */
-        
+
+    }
+    
+    func setUpContent() {
+        // 画面中央部
+        let contentView = UIView()
+        // size
+        contentView.frame.size = CGSize(width: view.frame.size.width, height: 340)
+        // 中心配置
+        contentView.center = CGPoint(x: view.center.x, y: view.center.y)
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 30
+        // 影の方向 width右 height下
+        contentView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        // 影の色 CGcolor型に
+        contentView.layer.shadowColor = UIColor.gray.cgColor
+        // 影の透明度
+        contentView.layer.shadowOpacity = 0.5
+        view.addSubview(contentView)
+        // 画面下部の色
+        view.backgroundColor = .systemGray6
     }
 
 
