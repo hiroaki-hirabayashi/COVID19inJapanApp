83e954038d5071009873006f315511a51d719a34
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-05-12 23:07:35 +0900

    チャート画面 / UISegmentController配置

--- COVID19inJapanApp/ViewController/ChartViewController.swift
+++ COVID19inJapanApp/ViewController/ChartViewController.swift
@@ -2,17 +2,67 @@
 //  ChartViewController.swift
 //  COVID19inJapanApp
 //
-//  Created by 平林 宏淳 on 2021/05/12.
+//  Created by 平林 宏淳 on 2021/05/08.
 //
 
 import UIKit
 
 class ChartViewController: UIViewController {
 
+    let colors = Colors()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60)
+        gradientLayer.colors = [colors.bluePurple.cgColor,
+                                colors.blue.cgColor,]
+        gradientLayer.startPoint = CGPoint.init(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint.init(x: 1, y: 1)
+        // レイヤー(CAGradientLayer)を載せる
+        view.layer.insertSublayer(gradientLayer, at: 0)
+//
+//        let backButton = UIButton(type: .system)
+//        backButton.frame = CGRect(x: 10, y: 30, width: 20, height: 20)
+//        backButton.setImage(UIImage(named: ""), for: .normal)
+//        backButton.tintColor = colors.white
+//        backButton.titleLabel?.font = .systemFont(ofSize: 20)
+//        backButton.addTarget(self, action: #selector(), for: .touchUpInside)
+//
+        let nextButton = UIButton(type: .system)
+        nextButton.frame = CGRect(x: view.frame.size.width - 105, y: 25, width: 100, height: 30)
+        nextButton.setTitle("円グラフ", for: .normal)
+        nextButton.setTitleColor(colors.white, for: .normal)
+        nextButton.titleLabel?.font = .systemFont(ofSize: 20)
+        nextButton.addTarget(self, action: #selector(goCircle), for: .touchUpInside)
+        view.addSubview(nextButton)
+        
+        let segment = UISegmentedControl(items: ["感染者数", "PCR数", "死者数"])
+        segment.frame = CGRect(x: 10, y: 70, width: view.frame.size.width - 20, height: 20)
+        segment.selectedSegmentTintColor = colors.blue
+        segment.selectedSegmentIndex = 0
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: colors.bluePurple], for: .normal)
+        segment.addTarget(self, action: #selector(segmentSwitchAction), for: .valueChanged)
+        view.addSubview(segment)
+    }
+    
+    @objc func goCircle() {
+        print("goCircle")
+    }
+    
+    @objc func segmentSwitchAction(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                print("感染者数")
+            case 1:
+                print("PCR数")
+            case 2:
+                print("死者数")
+            default:
+                break
+        }
     }
     
 
