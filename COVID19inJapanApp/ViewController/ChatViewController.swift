467efb9a29195cc390ecb05e99aec51214027e40
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-06-14 21:36:32 +0900

    update: Chatファイル追加

---
+++ COVID19inJapanApp/ViewController/ChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatViewController.swift
+//  COVID19inJapanApp
+//
+//  Created by 平林 宏淳 on 2021/06/14.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
