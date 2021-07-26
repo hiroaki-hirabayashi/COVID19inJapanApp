b0fdb064fbd8ee6e130bf429f02e1bfe75829a6d
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-07-27 01:51:29 +0900

    update: MessageKitプロトコル設定

--- COVID19inJapanApp/FirestoreData.swift
+++ COVID19inJapanApp/FirestoreData.swift
@@ -17,3 +17,38 @@ struct FirestoreData {
     var text: String?
     var userName: String?
 }
+
+//MessageKitのプロトコル 送信者のIDと名前の構造体
+struct Sender: SenderType {
+    var senderId: String
+    var displayName: String
+}
+//MessageKitのプロトコル
+struct Message: MessageType {
+    //送信するものがテキストなのか画像なのか動画なのか表すenum
+    var kind: MessageKind
+    //Senderプロトコルをメッセージ処理の際に代入する
+    var sender: SenderType
+    //メッセージの固有ID
+    var messageId: String
+    //日付情報
+    var sentDate: Date
+    
+    //Message構造体に値を代入する(ユーザーから投げられてくる情報をMessageKitのプロトコルに渡す)
+    private init(kind: MessageKind, sender: Sender, messageId: String, date: Date) {
+        self.kind = kind
+        self.sender = sender
+        self.messageId = messageId
+        self.sentDate = date
+    }
+    
+    //MessageKindがTextの時(String型のメッセージを送信する時) private initを呼ぶ
+    init(text: String, sender: Sender, messageId: String, date: Date) {
+        self.init(kind: .text(text), sender: sender, messageId: messageId, date: date)
+    }
+    
+    //MessageKindがNSAttributedString型の時
+    init(attributedText: NSAttributedString, sender: Sender, messageId: String, date: Date) {
+        self.init(kind: .attributedText(attributedText), sender: sender, messageId: messageId, date: date)
+    }
+}
